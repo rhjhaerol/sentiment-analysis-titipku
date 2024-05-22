8b8095df32ae8829dfe9c2e4b6d663e0e274e7e6
rhjhaerol <rhjhaerol@gmail.com>
2024-05-22 11:16:18 +0700

    update sentiment

--- sentiment.py
+++ sentiment.py
@@ -1,16 +1,12 @@
 import streamlit as st
 from wordcloud import WordCloud
-# import os
 import pandas as pd
 import seaborn as sns
-# import collections
 sns.set_theme(color_codes=True)
-import numpy as np
 import matplotlib.pyplot as plt
 import re
 import emoji
 import nltk
-from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.corpus import wordnet
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
@@ -19,13 +15,13 @@ from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassifica
 TF_ENABLE_ONEDNN_OPTS=0
 
 nltk.download('punkt')
-nltk.download('stopwords')
 nltk.download('wordnet')
 
+st.set_page_config(page_title="Sentiment Analysis")
 st.title('Sentiment Analysis App Titipku')
 
 url = "https://scrapping-app-review.streamlit.app/"
-st.write("Follow this [link](%s) to scrapping data" % url)
+st.write("Follow this [link](%s) to scrapping dataset" % url)
 
 upload_file = st.file_uploader("Upload a CSV File", type="csv")
 added_stopwords = st.text_input("Enter Add Stopword (comma-separated)")
@@ -65,6 +61,7 @@ if upload_file is not None:
 
             return ' '.join(lemmatized_words)
 
+        # Bert Model
         pretrained= "mdhugol/indonesia-bert-sentiment-classification"
         model = AutoModelForSequenceClassification.from_pretrained(pretrained)
         tokenizer = AutoTokenizer.from_pretrained(pretrained)
@@ -87,7 +84,7 @@ if upload_file is not None:
 
         sentiment_counts = data['label'].value_counts()
 
-        # Plot a bar chart using seaborn
+        # Plot a distribution
         st.set_option('deprecation.showPyplotGlobalUse', False)
         sns.set(style="whitegrid")
         plt.figure(figsize=(8, 6))
@@ -100,27 +97,6 @@ if upload_file is not None:
         data['label'].value_counts().sort_index() \
         .plot(kind='pie', autopct='%1.1f%%', title='Percentage Label Reviews')
         st.pyplot()
-
-        # def generate_wordcloud(text):
-        #     wordcloud = WordCloud(width=800, height=400, background_color ='white', stopwords = None, min_font_size = 10).generate(text)
-        #     plt.figure(figsize=(10, 5))
-        #     plt.imshow(wordcloud, interpolation='bilinear')
-        #     plt.axis("off")
-        #     st.pyplot()
-        
-        # st.subheader('Wordcloud All Review')
-        # text = data['cleaned_text'].str.cat(sep=' ')
-        # generate_wordcloud(text)
-
-        # st.subheader('Wordcloud Negative Review')
-        # text_negative = data[data['label']=='Negative']
-        # text_negative = text_negative['cleaned_text'].str.cat(sep=' ')
-        # generate_wordcloud(text_negative)
-
-        # st.subheader('Wordcloud Positive Review')
-        # text_positive = data[data['label']=='Positive']
-        # text_positive = text_positive['cleaned_text'].str.cat(sep=' ')
-        # generate_wordcloud(text_positive)
 
         negative_reviews = data[data['label']=='Negative']
         negative_reviews_sorted = negative_reviews.sort_values(by='score', ascending=False).head(10)
@@ -168,38 +144,7 @@ if upload_file is not None:
         st.subheader("WordCloud for Positive Sentiment")
         st.image(positive_wordcloud_filename)
 
-        # with open(positive_wordcloud_filename, "rb") as file:
-        #     btn = st.download_button(
-        #             label="Download Wordcloud ",
-        #             data=file,
-        #             file_name="wordcloud_positive.png",
-        #             mime="image/png"
-        #         )
-
-        # # Bigrams Positive Sentiment
-        # words1 = positive_text.split()
-        # # Get bigrams
-        # bigrams = list(zip(words1, words1[1:]))
-
-        # # Count bigrams
-        # bigram_counts = collections.Counter(bigrams)
-
-        # # Get top 10 bigram counts
-        # top_bigrams = dict(bigram_counts.most_common(10))
-
-        # # Create bar chart
-        # plt.figure(figsize=(10, 7))
-        # plt.bar(range(len(top_bigrams)), list(top_bigrams.values()), align='center')
-        # plt.xticks(range(len(top_bigrams)), list(top_bigrams.keys()), rotation=90)
-        # plt.xlabel('Bigram Words')
-        # plt.ylabel('Count')
-        # plt.title(f"Top 10 Bigram for Positive Sentiment")
-        # # Save the entire plot as a PNG
-        # plt.tight_layout()
-        # plt.savefig("bigram_positive.png")
-        # st.subheader("Bigram for Positive Sentiment")
-        # st.image("bigram_positive.png")
-
+        # Generate WordCloud for negative sentiment
         negative_wordcloud = WordCloud(
             min_font_size=3, max_words=200, width=800, height=400,
             colormap='viridis', background_color='white'
@@ -213,30 +158,7 @@ if upload_file is not None:
         st.subheader("WordCloud for Negative Sentiment")
         st.image(negative_wordcloud_filename)
 
-        # # Bigrams negative Sentiment
-        # words2 = negative_text.split()
-        # # Get bigrams
-        # bigrams = list(zip(words2, words2[1:]))
-
-        # # Count bigrams
-        # bigram_counts = collections.Counter(bigrams)
-
-        # # Get top 10 bigram counts
-        # top_bigrams = dict(bigram_counts.most_common(10))
-
-        # # Create bar chart
-        # plt.figure(figsize=(10, 7))
-        # plt.bar(range(len(top_bigrams)), list(top_bigrams.values()), align='center')
-        # plt.xticks(range(len(top_bigrams)), list(top_bigrams.keys()), rotation=90)
-        # plt.xlabel('Bigram Words')
-        # plt.ylabel('Count')
-        # plt.title(f"Top 10 Bigram for Negative Sentiment")
-        # # Save the entire plot as a PNG
-        # plt.tight_layout()
-        # plt.savefig("bigram_negative.png")
-        # st.subheader("Bigram for Negative Sentiment")
-        # st.image("bigram_negative.png")
-
+        # Generate WordCloud for neutral sentiment
         neutral_wordcloud = WordCloud(
             min_font_size=3, max_words=200, width=800, height=400,
             colormap='viridis', background_color='white'
@@ -249,27 +171,3 @@ if upload_file is not None:
         # Display the saved WordCloud image using Streamlit
         st.subheader("WordCloud for Neutral Sentiment")
         st.image(neutral_wordcloud_filename)
-
-        # # Bigrams neutral Sentiment
-        # words3 = neutral_text.split()
-        # # Get bigrams
-        # bigrams = list(zip(words3, words3[1:]))
-
-        # # Count bigrams
-        # bigram_counts = collections.Counter(bigrams)
-
-        # # Get top 10 bigram counts
-        # top_bigrams = dict(bigram_counts.most_common(10))
-
-        # # Create bar chart
-        # plt.figure(figsize=(10, 7))
-        # plt.bar(range(len(top_bigrams)), list(top_bigrams.values()), align='center')
-        # plt.xticks(range(len(top_bigrams)), list(top_bigrams.keys()), rotation=90)
-        # plt.xlabel('Bigram Words')
-        # plt.ylabel('Count')
-        # plt.title(f"Top 10 Bigram for Neutral Sentiment")
-        # # Save the entire plot as a PNG
-        # plt.tight_layout()
-        # plt.savefig("bigram_neutral.png")
-        # st.subheader("Bigram for Neutral Sentiment")
-        # st.image("bigram_neutral.png")
